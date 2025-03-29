e53b3cdda45a98617f50aaede3ad5eb842657d39
Your Name <vijaynaren@outlook.com>
2025-03-29 15:45:26 +0530

    code not completed yet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import Header from './components/Header';
 import Home from './components/Home';
-import About from "./components/About";
-import Skills from './components/Skills';
-import Services from './components/Services';
-import Work from './components/Work';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      
       <main className="main">
-        <Home />
-        <About />
-        <Skills />
-        <Services />
-        <Work />
-        <Contact />
+        <Home/>
       </main>
-
-      <Footer />
     </div>
   );
 }
